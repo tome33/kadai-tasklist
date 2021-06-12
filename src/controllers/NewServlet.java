62d9a716cb9a585575a4ed818dfd3ffdc097af48
ryohei.fukudome <gev421372@s.okadai.jp>
2021-06-12 23:46:35 +0900

    Add NewServlet.java

--- src/controllers/NewServlet.java
+++ src/controllers/NewServlet.java
@@ -1,20 +1,25 @@
 package controllers;
 
 import java.io.IOException;
+import java.sql.Timestamp;
 
+import javax.persistence.EntityManager;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import model.Task;
+import util.DBUtil;
+
 /**
  * Servlet implementation class NewServlet
  */
 @WebServlet("/new")
 public class NewServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
+    private static final long serialVersionUID = 1L;
+
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -22,10 +27,32 @@ public class NewServlet extends HttpServlet {
         super();
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	}
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        EntityManager em = DBUtil.createEntityManager();
+
+        // Taskのインスタンスを作成
+        Task tsk = new Task();
+
+        // tskの各フィールドにデータを代入
+        String content = "タスク１";
+        tsk.setContent(content);
+
+        Timestamp currentTime = new Timestamp(System.currentTimeMillis());		//現在の日時を取得
+        tsk.setCreated_at(currentTime);
+        tsk.setUpdated_at(currentTime);
+
+        // データベースに保存
+        em.getTransaction().begin();
+        em.persist(tsk);
+        em.getTransaction().commit();
+
+        // 自動採番されたIDの値を表示
+        response.getWriter().append(Integer.valueOf(tsk.getId()).toString());
+
+        em.close();
+    }
 
 }
