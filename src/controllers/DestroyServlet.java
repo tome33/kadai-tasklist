9c4f067f6d3945217c824f905442030305b46ab2
ryohei.fukudome <gev421372@s.okadai.jp>
2021-06-15 20:23:53 +0900

    Add DestroyServlet.java

--- src/controllers/DestroyServlet.java
+++ src/controllers/DestroyServlet.java
@@ -2,19 +2,23 @@ package controllers;
 
 import java.io.IOException;
 
+import javax.persistence.EntityManager;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import models.Task;
+import util.DBUtil;
+
 /**
  * Servlet implementation class DestroyServlet
  */
 @WebServlet("/destroy")
 public class DestroyServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
+    private static final long serialVersionUID = 1L;
+
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -22,10 +26,30 @@ public class DestroyServlet extends HttpServlet {
         super();
     }
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	}
+    /**
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+        // CSR対策（_token に値がセットされていなかったりセッションIDと値が異なったりしたらデータの登録ができないようにしています。）
+        String _token = request.getParameter("_token");
+        if(_token != null && _token.equals(request.getSession().getId())) {
+            EntityManager em = DBUtil.createEntityManager();
+
+            // セッションスコープからタスク内容のIDを取得して
+            // 該当のIDのタスク内容1件のみをデータベースから取得
+            Task tsk = em.find(Task.class, (Integer)(request.getSession().getAttribute("task_id")));
+
+            em.getTransaction().begin();
+            em.remove(tsk);       // データ削除
+            em.getTransaction().commit();
+            em.close();
+
+            // セッションスコープ上の不要になったデータを削除
+            request.getSession().removeAttribute("task_id");
+
+            // indexページへリダイレクト
+            response.sendRedirect(request.getContextPath() + "/index");
+        }
+    }
 }
